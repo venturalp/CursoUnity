73bd7aae94e60d6107193b209fbd2c89715977d1
Guilherme  Ventura <guilherme@fanatee.com>
2015-08-06 18:45:31 -0300

    Aula 9

---
+++ Aula 5/Aula 05/Assets/Scripts/aula.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aula : MonoBehaviour {
+
+	//Declarando variáveis
+	public	int 	valor;
+	private string 	nome;
+	public 	bool	flag;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("teste");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

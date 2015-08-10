f38cb9d32a52d1170c2f130d16103f50505de56d
Guilherme  Ventura <guilherme@fanatee.com>
2015-08-10 14:10:06 -0300

    Movimentação e Flip

--- Aula 5/Aula 05/Assets/Scripts/aula.cs
+++ Aula 5/Aula 05/Assets/Scripts/aula.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class aula : MonoBehaviour {
 
 	//Declarando variáveis
-	public	bool walk;
-	public Animator anima;
-	public float movimentacaoX;
+	public	bool 		walk;
+	public 	Animator 	anima;
+	public 	float		maxSpeed;
+	public  Rigidbody2D rbPlayer;
+	private float 		movimentacaoX;
+	private bool		facingRight = true;  
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +19,25 @@ public class aula : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetAxis ("Horizontal") != 0)
-			walk = true;
-		else
-			walk = false;
+		movimentacaoX = Input.GetAxis ("Horizontal");
 
+		if (movimentacaoX > 0 && !facingRight)
+			Flip ();
+		else if (movimentacaoX < 0 && facingRight)
+			Flip ();
+
+		rbPlayer.velocity = new Vector2 (movimentacaoX * maxSpeed, 0);
+
+		walk = movimentacaoX != 0;
 		anima.SetBool("walk", walk);
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 playerScale = transform.localScale;
+
+		playerScale.x *= -1;
+		transform.localScale = playerScale;
+
 	}
 }
